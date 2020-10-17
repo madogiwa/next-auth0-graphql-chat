1648e9c9e419936b0b0d4d2eca4bebcfcd83ebc7
Hidenori Sugiyama <madogiwa@gmail.com>
2020-10-17 13:48:52 +0900

    Update @apollo/link-context

--- src/client/provider.tsx
+++ src/client/provider.tsx
@@ -1,5 +1,5 @@
 import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
+import { setContext } from '@apollo/link-context'
 import { useAuth0 } from '@auth0/auth0-react'
 
 const MyApolloProvider = ({ children }) => {
